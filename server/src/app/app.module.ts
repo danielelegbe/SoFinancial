98d8afc91d06c7b237e6b868b242d07036af775a
Daniel Elegbe <daniel_elegbe@hotmail.co.uk>
2021-09-21 22:33:23 +0100

    refactor app module and creat auth module

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from '../auth/auth.module';
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })

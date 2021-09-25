8590f79ca3992dc2b405871f976ce088e8a4feaf
Daniel Elegbe <daniel_elegbe@hotmail.co.uk>
2021-09-25 11:06:01 +0100

    add head to pages

--- client/pages/forum/[forumName]/[postId].tsx
+++ client/pages/forum/[forumName]/[postId].tsx
@@ -9,6 +9,7 @@ import { Avatar, Button } from '@chakra-ui/react';
 import Comment from '../../../components/Comments/Comment';
 import DeletePostModal from '../../../components/Modals/DeletePostModal';
 import AddCommentModal from '../../../components/Modals/AddCommentModal';
+import Head from 'next/head';
 
 const FullPost = () => {
   const user = useMeQuery().data?.me;
@@ -19,56 +20,72 @@ const FullPost = () => {
 
   if (error) {
     return (
-      <Heading p={8}>
-        Page not found. Go to{' '}
-        <Link as={NextLink} href="/">
-          <Text
-            _hover={{ cursor: 'pointer' }}
-            display="inline"
-            color="blue.500"
-          >
-            Home?
-          </Text>
-        </Link>
-      </Heading>
+      <>
+        <Head>
+          <title>Page not found</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+        <Heading p={8}>
+          Page not found. Go to{' '}
+          <Link as={NextLink} href="/">
+            <Text
+              _hover={{ cursor: 'pointer' }}
+              display="inline"
+              color="blue.500"
+            >
+              Home?
+            </Text>
+          </Link>
+        </Heading>
+      </>
     );
   }
 
   if (!data || loading) return <Progress size="sm" isIndeterminate />;
   const post = data.getPostById!;
   return (
-    <Box>
-      <Stack spacing={20} px={12} pb={8} mt="20" boxShadow="md">
-        <Stack>
-          <Heading textColor="blue.700">{post.title}</Heading>
-        </Stack>
-        <Box>{post.content}</Box>
-        <Flex align="center">
-          <NextLink href={`/users/${post.author?.username}`} passHref>
-            <Text
-              as="a"
-              _hover={{ textDecoration: 'underline', cursor: 'pointer' }}
-            >
-              Posted by - {post.author?.username}
-            </Text>
-          </NextLink>
-          <Avatar src={post.author?.avatar} size="xs" ml={2} />
+    <>
+      <Head>
+        <title>{post.title}</title>
 
-          {user?.id === post.author?.id ? (
-            <DeletePostModal postId={post.id} />
-          ) : null}
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Box>
+        <Stack spacing={20} px={12} pb={8} mt="20" boxShadow="md">
+          <Stack>
+            <Heading textColor="blue.700">{post.title}</Heading>
+          </Stack>
+          <Box>{post.content}</Box>
+          <Flex align="center">
+            <NextLink href={`/users/${post.author?.username}`} passHref>
+              <Text
+                as="a"
+                _hover={{ textDecoration: 'underline', cursor: 'pointer' }}
+              >
+                Posted by - {post.author?.username}
+              </Text>
+            </NextLink>
+            <Avatar src={post.author?.avatar} size="xs" ml={2} />
+
+            {user?.id === post.author?.id ? (
+              <DeletePostModal postId={post.id} />
+            ) : null}
+          </Flex>
+        </Stack>
+        <Flex ml={12} mt={20} align="center">
+          <Heading as="h3">Comments</Heading>
+          {user && <AddCommentModal postId={post.id} />}
         </Flex>
-      </Stack>
-      <Flex ml={12} mt={20} align="center">
-        <Heading as="h3">Comments</Heading>
-        {user && <AddCommentModal postId={post.id} />}
-      </Flex>
-      <Stack spacing={10}>
-        {post.comments?.map((comment) => (
-          <Comment key={comment.id} {...comment} />
-        ))}
-      </Stack>
-    </Box>
+        <Stack spacing={10}>
+          {post.comments?.map((comment) => (
+            <Comment key={comment.id} {...comment} />
+          ))}
+        </Stack>
+      </Box>
+    </>
   );
 };
 

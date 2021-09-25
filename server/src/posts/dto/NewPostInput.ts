c7eee7fd028f4c593835dd2bb84875ae2511bd19
Daniel Elegbe <daniel_elegbe@hotmail.co.uk>
2021-09-25 17:51:48 +0100

    refactor create post

--- server/src/posts/dto/NewPostInput.ts
+++ server/src/posts/dto/NewPostInput.ts
@@ -1,18 +1,21 @@
 import { Field, InputType } from '@nestjs/graphql';
-import { IsString, MaxLength } from 'class-validator';
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
 @InputType()
 export class NewPostInput {
   @IsString()
   @MaxLength(255)
+  @IsNotEmpty()
   @Field()
   title: string;
 
   @IsString()
+  @IsNotEmpty()
   @Field()
   content: string;
 
   @IsString()
+  @IsNotEmpty()
   @Field()
   forum: string;
 }

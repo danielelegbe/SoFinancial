3524aaafa4457f0ebc9edcb4c62d5922762df422
Daniel Elegbe <daniel_elegbe@hotmail.co.uk>
2021-09-25 17:34:15 +0100

    refactor redux store

--- client/features/user/userSlice.ts
+++ client/features/user/userSlice.ts
@@ -4,29 +4,18 @@ import type { RootState } from '../../app/store';
 // Define a type for the slice state
 interface LoggedInUser {
   access_token: string;
-  id: number | null;
 }
 
 // Define the initial state using that type
 
 const initialState: LoggedInUser = {
   access_token: '',
-  id: null,
 };
 
 export const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    setUser: (
-      state: LoggedInUser,
-      { payload }: PayloadAction<LoggedInUser>
-    ) => {
-      return {
-        ...state,
-        access_token: payload.access_token,
-      };
-    },
     setAccessToken: (
       state: LoggedInUser,
       { payload }: PayloadAction<string>
@@ -36,7 +25,7 @@ export const userSlice = createSlice({
   },
 });
 
-export const { setUser, setAccessToken } = userSlice.actions;
+export const { setAccessToken } = userSlice.actions;
 
 export const selectUser = (state: RootState) => state.user;
 

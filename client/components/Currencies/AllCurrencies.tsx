5a8f94888d6f9d08fb58b9603d9dcb9ac33a5996
Daniel Elegbe <daniel_elegbe@hotmail.co.uk>
2021-09-27 08:23:57 +0100

    add USDCAD

--- client/components/Currencies/AllCurrencies.tsx
+++ client/components/Currencies/AllCurrencies.tsx
@@ -13,16 +13,15 @@ const Currencies = () => {
   const [BTC, setBTC] = useState<ICurrency | null>(null);
   const [GBPUSD, setGBPUSD] = useState<ICurrency | null>(null);
   const [EURUSD, setEURUSD] = useState<ICurrency | null>(null);
+  const [USDCAD, setUSDCAD] = useState<ICurrency | null>(null);
 
   useEffect(() => {
     const socket = io();
     axios('/api/currencies').finally(() => {
       io('/');
     });
-    socket.on('connect', () => console.log('connect'));
     socket.on('currencies', (data) => {
       const currencies = JSON.parse(data);
-      //   console.log(currencies);
 
       if (currencies?.data) {
         const BTC: ICurrency = currencies.data.find(
@@ -34,9 +33,13 @@ const Currencies = () => {
         const EURUSD: ICurrency = currencies.data.find(
           (currency: ICurrency) => currency.s === 'OANDA:EUR_USD'
         );
+        const USDCAD: ICurrency = currencies.data.find(
+          (currency: ICurrency) => currency.s === 'OANDA:USD_CAD'
+        );
         BTC && setBTC(BTC);
         GBPUSD && setGBPUSD(GBPUSD);
         EURUSD && setEURUSD(EURUSD);
+        USDCAD && setUSDCAD(USDCAD);
       }
     });
 
@@ -47,11 +50,12 @@ const Currencies = () => {
 
   return (
     <Flex justify="center" boxShadow="md" py={2} align="center" bg="gray.100">
-      {!BTC && <CircularProgress isIndeterminate color="blue.700" />}
+      {!BTC && <CircularProgress isIndeterminate color="blue.500" />}
       <HStack spacing={5}>
         {BTC && <Currency {...BTC} />}
         {GBPUSD && <Currency {...GBPUSD} />}
         {EURUSD && <Currency {...EURUSD} />}
+        {USDCAD && <Currency {...USDCAD} />}
       </HStack>
     </Flex>
   );

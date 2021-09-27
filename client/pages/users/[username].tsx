feab62b5bfc030d635394ad14ed9008fc41cb276
Daniel Elegbe <daniel_elegbe@hotmail.co.uk>
2021-09-27 21:29:18 +0100

    add head to profile page

--- client/pages/users/[username].tsx
+++ client/pages/users/[username].tsx
@@ -13,6 +13,7 @@ import React from 'react';
 import { useGetProfileQuery } from '../../generated/graphql';
 import withApollo from '../../lib/withApollo';
 import Post from '../../components/Posts/Post';
+import Head from 'next/head';
 
 const UserPage = () => {
   const router = useRouter();
@@ -34,20 +35,25 @@ const UserPage = () => {
   const user = data.getProfile;
 
   return (
-    <Flex direction="column" align="center">
-      <HStack spacing={4}>
-        <Heading as="h2">{user.username}</Heading>
-        <Avatar src={user.avatar} />
-      </HStack>
-      <Heading mt={10} as="h3">
-        Posts
-      </Heading>
-      <Stack align="center">
-        {user.posts?.map((post) => (
-          <Post key={post.id} {...post} />
-        ))}
-      </Stack>
-    </Flex>
+    <>
+      <Head>
+        <title>{user.username}</title>
+      </Head>
+      <Flex direction="column" align="center">
+        <HStack spacing={4}>
+          <Heading as="h2">{user.username}</Heading>
+          <Avatar src={user.avatar} />
+        </HStack>
+        <Heading mt={10} as="h3">
+          Posts
+        </Heading>
+        <Stack align="center">
+          {user.posts?.map((post) => (
+            <Post key={post.id} {...post} />
+          ))}
+        </Stack>
+      </Flex>
+    </>
   );
 };
 

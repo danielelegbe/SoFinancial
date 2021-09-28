9ce205ae1cfd676ad60578b8d06314e188458138
Daniel Elegbe <daniel_elegbe@hotmail.co.uk>
2021-09-28 15:46:29 +0100

    add new currencies and color based on last price

--- client/components/Currencies/AllCurrencies.tsx
+++ client/components/Currencies/AllCurrencies.tsx
@@ -6,14 +6,15 @@ import Currency from './Currency';
 
 export interface ICurrency {
   s?: string;
-  p?: number;
+  p?: number | null;
 }
 
 const Currencies = () => {
   const [BTC, setBTC] = useState<ICurrency | null>(null);
   const [GBPUSD, setGBPUSD] = useState<ICurrency | null>(null);
   const [EURUSD, setEURUSD] = useState<ICurrency | null>(null);
-  const [USDCAD, setUSDCAD] = useState<ICurrency | null>(null);
+  const [APPLE, setAPPLE] = useState<ICurrency | null>(null);
+  const [TESLA, setTESLA] = useState<ICurrency | null>(null);
 
   useEffect(() => {
     const socket = io();
@@ -33,13 +34,17 @@ const Currencies = () => {
         const EURUSD: ICurrency = currencies.data.find(
           (currency: ICurrency) => currency.s === 'OANDA:EUR_USD'
         );
-        const USDCAD: ICurrency = currencies.data.find(
-          (currency: ICurrency) => currency.s === 'OANDA:USD_CAD'
+        const APPLE: ICurrency = currencies.data.find(
+          (currency: ICurrency) => currency.s === 'AAPL'
+        );
+        const TESLA: ICurrency = currencies.data.find(
+          (currency: ICurrency) => currency.s === 'TSLA'
         );
         BTC && setBTC(BTC);
         GBPUSD && setGBPUSD(GBPUSD);
         EURUSD && setEURUSD(EURUSD);
-        USDCAD && setUSDCAD(USDCAD);
+        APPLE && setAPPLE(APPLE);
+        TESLA && setTESLA(TESLA);
       }
     });
 
@@ -51,11 +56,12 @@ const Currencies = () => {
   return (
     <Flex justify="center" boxShadow="md" py={2} align="center" bg="gray.100">
       {!BTC && <CircularProgress isIndeterminate color="blue.500" />}
-      <HStack spacing={5}>
+      <HStack spacing={2}>
         {BTC && <Currency {...BTC} />}
         {GBPUSD && <Currency {...GBPUSD} />}
         {EURUSD && <Currency {...EURUSD} />}
-        {USDCAD && <Currency {...USDCAD} />}
+        {APPLE && <Currency {...APPLE} />}
+        {TESLA && <Currency {...TESLA} />}
       </HStack>
     </Flex>
   );
--- client/components/Currencies/Currency.tsx
+++ client/components/Currencies/Currency.tsx
@@ -1,8 +1,34 @@
-import { HStack, Text } from '@chakra-ui/react';
-import React from 'react';
+import { MinusIcon, TriangleDownIcon, TriangleUpIcon } from '@chakra-ui/icons';
+import { ComponentWithAs, HStack, IconProps, Text } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../app/store';
+import { setLastPrice } from '../../features/currencies/currenciesSlice';
 import { ICurrency } from './AllCurrencies';
 
 const Currency = ({ s: symbol, p: price }: ICurrency) => {
+  const [textColor, setTextColor] = useState('');
+  const [icon, setIcon] = useState(<MinusIcon />);
+  const dispatch = useDispatch();
+
+  const { p: lastPrice } = useSelector((state: RootState) => state.currency);
+
+  useEffect(() => {
+    if (!lastPrice || lastPrice === price) {
+      setTextColor('black');
+      setIcon(<MinusIcon boxSize={3} />);
+    } else {
+      if (price && price > lastPrice) {
+        setTextColor('green');
+        setIcon(<TriangleUpIcon color="green" boxSize={3} />);
+      } else {
+        setTextColor('red');
+        setIcon(<TriangleDownIcon color="red" boxSize={3} />);
+      }
+    }
+    price && dispatch(setLastPrice(price));
+  }, [price]);
+
   return (
     <HStack
       border="1px"
@@ -15,7 +41,10 @@ const Currency = ({ s: symbol, p: price }: ICurrency) => {
       <Text fontSize="sm" fontWeight="bold">
         {symbol}
       </Text>
-      <Text fontSize="sm">{price && parseFloat(price.toFixed(5))}</Text>
+      <Text fontSize="sm" color={textColor}>
+        {price && parseFloat(price.toFixed(5))}
+      </Text>
+      {icon}
     </HStack>
   );
 };
